31f8bf41b7491d641aede9d4fb12c69096dd06f6
TanzimaAkhanda <57340742+TanzimaAkhanda@users.noreply.github.com>
2020-06-13 22:56:03 +0600

    Create Task 8: Least Common Multiple.c

---
+++ Task 8: Least Common Multiple.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main(){
+    int  a, b ,gcd, rem,lcm,n1,n2;
+    scanf("%d %d",&n1,&n2);
+    a=n1;
+    b=n2;
+    while(b!=0)
+    {
+       rem=a%b;
+       a=b;
+       b=rem;
+    }
+    gcd=a;
+    lcm= ((n1*n2)/gcd);
+    printf("%d\n",lcm);
+    return 0;
+}\
